4fd4e2ea159d2f9eef16ee98d448f7ff0f6cb9d4
irosales27 <113443176+irosales27@users.noreply.github.com>
2023-01-12 13:14:35 -0500

    Update sketch.js

--- CreativeCoding/Grid_GIF/sketch.js
+++ CreativeCoding/Grid_GIF/sketch.js
@@ -3,13 +3,13 @@ let img; // img for static image
 
 
 function preload () {
-  gif = loadImage (assets/StickFigureGraphic.png); // to preload my image/GIF
+  img = loadImage (assets/StickFigureGraphic.png); // to preload my image/GIF
 }
 
 function setup() { //runs 1 time
 createCanvas (windowWidth,windowHeight); //canvas is 720 pixels x 720 pixels
-  background (255,0 ,0); //background color
 }
+
 function draw() { //runs in a loop
 fill (0,150,0) //fills shape with RGB color
 strokeWeight (1) // stroke weight (thickness) 1 pixel
