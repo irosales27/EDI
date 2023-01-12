e1b6a6dedaec2e76c75e83b3d9fe264c950771fa
irosales27 <113443176+irosales27@users.noreply.github.com>
2023-01-12 12:58:40 -0500

    edit

--- CreativeCoding/Grid_GIF/sketch.js
+++ CreativeCoding/Grid_GIF/sketch.js
@@ -8,7 +8,7 @@ function preload () {
 
 function setup() { //runs 1 time
 createCanvas (windowWidth,windowWidth); //canvas is 720 pixels x 720 pixels
-  background (0,0,255); //background color
+  background (50,100,255); //background color
 }
 function draw() { //runs in a loop
 fill (0,150,0) //fills shape with RGB color
@@ -17,7 +17,7 @@ strokeWeight (1) // stroke weight (thickness) 1 pixel
 var num =  30;  // number of squares in my array
 var sideLen = windowWidth/num; // side length
 
-for (var y = 0; y < windowWidth; y = y + sideLen) { // loop creates a row in the y direction
+for (var y = 0; y < windowHeight; y = y + sideLen) { // loop creates a row in the y direction
 for (var x = 0; x < windowWidth; x = x + sideLen) { // loop creates a row in the x direction
         image (img, x, y, windowWidth/num, windowWidth/num);
       }
